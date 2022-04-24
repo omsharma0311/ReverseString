e9bcbd786778b3472bf7ae7b4b422f5914bff815
OM SHARMA <osharma311@hotmail.com>
2022-04-24 14:16:03 +0530

    Add project files.

---
+++ reverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace reverseString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine();
+
+            Console.WriteLine("Original string");
+            Console.WriteLine(str);
+
+            Console.WriteLine("Reversed String");
+
+            var count = 0;
+            foreach (var num in str) 
+                count++;
+            //var count = str.Length-1;
+
+            for (int j =count-1;j>=0;j--)
+            {
+                Console.Write(str[j]);
+            }
+        }
+    }
+}
